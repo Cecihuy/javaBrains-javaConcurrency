f595a98378ecb91a98f6e98cb811fa0530a654fe
sourceTree <sourcetree@mail.com>
2024-01-07 21:08:07 -0700

    07 Thread Syntax alternatives

--- Hello.java
+++ Hello.java
@@ -1,14 +1,19 @@
 class Hello{
     public static void main(String[] args) {
-        Runnable r = new MyRunnable();
-        // r.run();
+        Runnable r = () -> System.out.println("I am running!");
         Thread t = new Thread(r);
         t.start();
+        
+        //or
+        new Thread(() -> System.out.println("I am running! in one line")).start();
+        
+        //or extends Thread in custom class
+        new MyThread().start();
     }
 }
-class MyRunnable implements Runnable{
+class MyThread extends Thread{
     @Override
-    public void run() {
-        System.out.println("I am running!");
-    }    
+    public void run(){
+        System.out.println("I am running! from custom class extends Thread");
+    }
 }
