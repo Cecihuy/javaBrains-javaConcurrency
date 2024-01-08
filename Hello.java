a389a5554a323a76138716d5f05c010af7c3dda5
sourceTree <sourcetree@mail.com>
2024-01-08 03:00:42 -0700

    11 Coding with Thread States

--- Hello.java
+++ Hello.java
@@ -1,14 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 class Hello{
     public static void main(String[] args) {
-        int number;
+        List<Thread> threads = new ArrayList<>();
+
+        Runnable statusReporter = () -> {
+            while(true){
+                printThreads(threads);
+            }
+        };
+
+        Thread reporterThread = new Thread(statusReporter);
+        reporterThread.start();
 
         while (true) {
             Scanner sc = new Scanner(System.in);
             System.out.println("\n I can tell you the nth prime number. Enter n: ");
             int n = sc.nextInt();
-            if (n == 0) break;
+            if (n == 0) {
+                reporterThread.interrupt();
+                break;
+            }
 
             Runnable r = new Runnable() {
                 @Override
@@ -19,11 +33,20 @@ class Hello{
                 }
             };
             Thread t = new Thread(r);
+            threads.add(t);
             t.setDaemon(true);
             t.start();
         }
     }
+    private static void printThreads(List<Thread> threads) {
+        System.out.print("\n Threads status: ");
+        threads.forEach(thread -> {
+            System.out.print(thread.getState() + " ");
+        });
+        System.out.println("");
+    }
 }
+
 class PrimeNumberUtil{
     public static int calculatePrime(int n) {
         int number;
